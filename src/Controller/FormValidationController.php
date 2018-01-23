aeac570f8a201e18e8ed2074fb3345eabc5c1d1e
pepezp <yy.lesage@wanadoo.fr>
2018-01-23 13:56:39 +0100

    MAJ page formulaire

--- src/Controller/FormValidationController.php
+++ src/Controller/FormValidationController.php
@@ -7,14 +7,51 @@ use Silex\Application;
 class FormValidationController {
 
     public function formValidationAction(Application $app) {
-       
-        return $app['twig']->render('form_validation.twig');
-    }
-    public function SaveFormAction(Application $app) {
-       $formation=$_POST[''];
-    }
 
-}
+       $formation=(int)($_POST['selectbasic']);//valeur de l'option sélectionner 
+
+       $smiley=(int)($_POST['action']); 
+       
+       $textarea=$_POST['textarea'];
+   
+
+        //vérif formulaire
+            $error=0;
+
+            if ($formation==0){
+                $error+=1;
+                
+            }else{
+                $error+=0;
+            }
+
+        //afficher messages erreur
+
+           if ($smiley==0){
+                $error+=1;
+            }else{
+                $error+=0;
+            }
+
+            if (($smiley==2 && $textarea=="") || ($smiley==3 && $textarea=="")){
+                $error+=1;
+            }else{
+                $error+=0;
+            }
+            
+            if ($error==0){
+
+        //Préparer la requête
+            $q = $app['db']->prepare(" INSERT INTO `given_answer` (`date`, `question_idquestion`, `chosen_smiley`, `why`, `formation_idformation`) VALUES (now(),1,'$smiley','$textarea','$formation')");
+   
+        // Envoyer la requête
+            $q->execute(array());
+            return $app['twig']->render('form_validation.twig');
+            
+            }else{
+            }
+    } 
+ }
 
 
 
--- web/js/formulaire_satisfaction.js
+++ web/js/formulaire_satisfaction.js
@@ -1,6 +1,6 @@
-// EN CHANTIER !!!!!!!!!!!!!!!!!!!!!!!
 
-//cliquer sur les smileys pour sélectionner
+  //cliquer sur les smileys pour sélectionner
+
 $('#happy').on('click', function(){
      $('radios-0').attr("checked",true);
      $('#happy').css('width','85%');
@@ -22,15 +22,43 @@ $('#unhappy').on('click', function(){
     $('#unhappy').css('width','85%');
 });
 
-//cookie id
 
+  
+    
+    //vérif formulaire fontcion blabla 
+        $('#button').on('click', function(e){
+            e.preventDefault();
+             console.log("toto");
+            $formation=parseInt($_POST['#selectbasic']); 
+               
+            
+            $smiley=parseInt($_POST['.smiley']); 
+                   
+            $textarea=$_POST['#textarea'];
 
-/* faire disparaître le formulaire au click "envoyer"
-$(document).ready(function(){
-    $('#button').click(function(){
-        $('.form-horizontal').hide();
-    });
-});
+            $error=0;
+           
+            if ($formation==0){
+                $error+=1;
+                $messError.css('visibility', 'visible');
+            }else{
+                $error+=0;
+                $messError.css('visibility', 'hidden');
+            }
+        
+        /*
+            if ($smiley==0){
 
+                $error+=1;
+            }else{
+                $error+=0;
+            }
+
+            if (($smiley==2 && $textarea=="") || ($smiley==3 && $textarea=="")){
+               
+                $error+=1;
+            }else{
+                $error+=0;
+            }
 */
-
+        });
