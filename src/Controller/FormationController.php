4a0ac766d48077936809403375ff639a9e3e0b87
cedric60 <cedric.desgroux@yahoo.fr>
2018-01-26 15:53:07 +0100

    page addformation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -16,5 +16,64 @@
             return $app['twig']->render('formation.twig', array('formationList' => $formationList));
         }
         public function AddFormationAction(Application $app){
+            $formationList = $app['dao.formation']->findAll();
+            $sessionList = $app['dao.formation']->findActiveSessions();
+            return $app['twig']->render('addformation.twig', array('formationList' => $formationList,
+                                                                'sessionList' => $sessionList));
+        }
+        public function saveFormationAction(Application $app){
+                $formationId = (int)($_POST['formation']);
+                $newformation =  $_POST['newformation']; 
+                $start = $_POST['start_date'];
+                $end = $_POST['end_date'];           
+                $error = false;
+    
+                var_dump($_POST);
+                if($formationId == 0){
+                    echo 'Veuillez selectionner une categorie';
+                    $error = true;
+                }
+                if($formationId == 9999){
+                    if($newformation == ""){
+                        echo 'Veuillez selectionner une formation';
+                        $error = true;
+                    }
+                }
+                if($start == ""){
+                    echo 'Veuillez selectionner une date de debut';
+                    $error = true;
+                }
+                if($end == ""){
+                    echo 'Veuillez selectionner une date de fin';
+                    $error = true;
+                }
+
+                
+                if($error == false && $formationId == 9999){               
+                    $q = $app['db']->prepare("INSERT INTO `formation`(`name`) VALUES ('$newformation')");
+                    $q->execute(array());
+                    $arrayformationId = $app['dao.formation']->findLastId();
+                    $idFormation = (int)($arrayformationId["MAX( idformation )"]);
+                    $sql = $app['db']->prepare("INSERT INTO `session`(`start_date`, `end_date`, `formation_idformation`) VALUES ('$start', '$end', '$idFormation')");
+                    $sql->execute(array());               
+                }
+                if($error == false && $formationId != 9999){
+                    $q = $app['db']->prepare("INSERT INTO `session`(`start_date`, `end_date`, `formation_idformation`) VALUES ('$start', '$end', '$formationId')");
+                    $q->execute(array());
+                }
+
+                
+                $formationList = $app['dao.formation']->findAll();
+                return $app['twig']->render('addformation.twig', array('formationList' => $formationList));
+                
+    
+            }
+        
+
+        public function sessionActiveAction($id, Application $app){
+            $formationList = $app['dao.formation']->findAll();
+            $sessionList = $app['dao.formation']->findActiveSession($id);
+
+            return $app->json($sessionList);
         }
     }
---
+++ web/js/addformation.js
@@ -0,0 +1,83 @@
+$('select#formation').on('change', function() {
+
+    if ($('select#formation option:selected').val() == 9999) {
+        $('#newformationdiv').css({
+            display: 'block'
+        })
+    }
+})
+
+$('#button').on('click', function(e) {
+
+    e.preventDefault();
+    var $selectFormation = $('select#formation option:selected').val(),
+        $newformation = $('#newformation').val(),
+        $start = $('#start_date').val(),
+        $end = $('#end_date').val();
+
+
+    $error = false;
+
+    if ($selectFormation == 0) {
+        document.getElementById('erreurformation').innerHTML = 'valeur incorrecte';
+        $('#formation').css({
+            borderColor: 'red',
+            color: 'red'
+        });
+        $error = true;
+    } else {
+        document.getElementById('erreurformation').innerHTML = '';
+        $('#formation').css({
+            borderColor: '#ced4da',
+            color: '#4976b9'
+        });
+    }
+    if ($selectFormation == 9999) {
+        if ($newformation == "") {
+            document.getElementById('erreurnewformation').innerHTML = 'valeur incorrecte';
+            $('#newformation').css({
+                borderColor: 'red',
+                color: 'red'
+            });
+            $error = true;
+        } else {
+            document.getElementById('erreurnewformation').innerHTML = '';
+            $('#newformation').css({
+                borderColor: '#ced4da',
+                color: '#4976b9'
+            });
+        }
+    }
+    if ($start == "") {
+        document.getElementById('erreurstart').innerHTML = 'valeur incorrecte';
+        $('#start_date').css({
+            borderColor: 'red',
+            color: 'red'
+        });
+        $error = true;
+    } else {
+        document.getElementById('erreurstart').innerHTML = 'valeur incorrecte';
+        $('#start_date').css({
+            borderColor: '#ced4da',
+            color: '#4976b9'
+        });
+    }
+    if ($end == "") {
+        document.getElementById('erreurend').innerHTML = 'valeur incorrecte';
+        $('#end_date').css({
+            borderColor: 'red',
+            color: 'red'
+        });
+        $error = true;
+    } else {
+        document.getElementById('erreurend').innerHTML = 'valeur incorrecte';
+        $('#end_date').css({
+            borderColor: '#ced4da',
+            color: '#4976b9'
+        });
+    }
+
+    if ($error == false) {
+        $('#addformationform').submit();
+    }
+})
